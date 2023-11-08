9a1191b42c778ef43ca90330449f98ca8aa9cd30
Kittonn <tonkittipod@gmail.com>
2023-11-09 02:17:32 +0700

    feat: import api url from .env

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5001/api/users/'
+const API_URL = `${process.env.REACT_APP_API_URL}/api/users/`
 
 // Register user
 const register = async (userData) => {
